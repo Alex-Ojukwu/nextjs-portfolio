74cf51632550ffcf525a0e208750ebc513daf0da
Alex Ojukwu <nduka.alex-ojukwu@pau.edu.ng>
2025-11-29 15:16:07 +0100

    image fix

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -63,13 +63,12 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-         <Image
+       <Image
   src="/images/ME.jpg"
   alt="hero image"
-  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full object-cover"
+  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full object-cover w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-[500px] lg:h-[500px]"
   width={500}
   height={500}
-  style={{ width: '500px', height: '500px' }}
 />
           </div>
         </motion.div>
